f7f99cf7ce78bb10aea6498ff5c49074eb117491
Max Ogden <max@maxogden.com>
2014-03-18 15:52:45 -0700

    1.0.1 - check if online or not

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var request = require('request')
 var cmd = require('child_process')
 var fs = require('fs')
+var hasInternet = require('hasinternet')
 
 var token = process.argv[2] || process.env['FSQTOKEN']
 var lastSsid = ""
@@ -18,66 +19,73 @@ function wait() {
 }
 
 function checkin() {
-  var ssids = JSON.parse(fs.readFileSync('venues.json'))
-  
-  // OSX only
-  var airport = "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I" +
-                " | awk '/ SSID/ {print substr($0, index($0, $2))}'"
-  
-  cmd.exec(airport, done)
-  
-  function done(err, stdo, stde) {
-    if (err) return console.error(err)
-    var ssid = stdo.trim()
-    
-    var fsqid = ssids[ssid]
-    if (!fsqid) {
-      wait()
-      return console.log(ssid, 'not in whitelist')
+  hasInternet(function(err, online) {
+    if (err || !online) {
+      console.log('Not online')
+      return wait()
     }
-    if (lastSsid === ssid) {
-      wait()
-      return console.log('Already checked in to', ssid)
-    }
+  
+    var ssids = JSON.parse(fs.readFileSync('venues.json'))
+  
+    // OSX only
+    var airport = "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I" +
+                  " | awk '/ SSID/ {print substr($0, index($0, $2))}'"
+  
+    cmd.exec(airport, done)
+  
+    function done(err, stdo, stde) {
+      if (err) return console.error(err)
+      var ssid = stdo.trim()
     
-    lastSsid = ssid
+      var fsqid = ssids[ssid]
+      if (!fsqid) {
+        wait()
+        return console.log(ssid, 'not in whitelist')
+      }
+      if (lastSsid === ssid) {
+        wait()
+        return console.log('Already checked in to', ssid)
+      }
     
-    getLastCheckin()
+      lastSsid = ssid
     
-    function getLastCheckin() {
-      var myCheckins = "https://api.foursquare.com/v2/users/self/checkins?oauth_token=" + token + "&v=" + dateStamp()
-      request(myCheckins, function(err, resp, body) {
-        if (err || resp.statusCode > 299) {
-          console.error(err || body)
-          return wait()
-        }
-        var lastCheckin = JSON.parse(body).response.checkins.items[0]
-        if (lastCheckin.venue.id === fsqid) {
-          if (~~(Date.now()/1000) - (+lastCheckin.createdAt) > checkinPeriod) {
-            doCheckin()
-          } else {
-            console.error('Already checked in recently to', ssid)
+      getLastCheckin()
+    
+      function getLastCheckin() {
+        var myCheckins = "https://api.foursquare.com/v2/users/self/checkins?oauth_token=" + token + "&v=" + dateStamp()
+        request(myCheckins, function(err, resp, body) {
+          if (err || resp.statusCode > 299) {
+            console.error(err || body)
             return wait()
           }
-        } else {
-          doCheckin()
-        }
-      })
-    }
+          var lastCheckin = JSON.parse(body).response.checkins.items[0]
+          if (lastCheckin.venue.id === fsqid) {
+            if (~~(Date.now()/1000) - (+lastCheckin.createdAt) > checkinPeriod) {
+              doCheckin()
+            } else {
+              console.error('Already checked in recently to', ssid)
+              return wait()
+            }
+          } else {
+            doCheckin()
+          }
+        })
+      }
 
-    function doCheckin() {
-      var checkinUrl = "https://api.foursquare.com/v2/checkins/add?oauth_token=" + token + "&venueId=" + fsqid + "&v=" + dateStamp()
-      console.log("Checking in at", ssid, checkinUrl)
-      request.post(checkinUrl, function(err, resp, body) {
-        if (err || resp.statusCode > 299) {
-          console.error(err || body)
-        } else {
-          console.log('Checked in at', JSON.parse(body).response.checkin.venue.name)
-        } 
-        wait()
-      })
+      function doCheckin() {
+        var checkinUrl = "https://api.foursquare.com/v2/checkins/add?oauth_token=" + token + "&venueId=" + fsqid + "&v=" + dateStamp()
+        console.log("Checking in at", ssid, checkinUrl)
+        request.post(checkinUrl, function(err, resp, body) {
+          if (err || resp.statusCode > 299) {
+            console.error(err || body)
+          } else {
+            console.log('Checked in at', JSON.parse(body).response.checkin.venue.name)
+          } 
+          wait()
+        })
+      }
     }
-  }
+  })
 }
 
 function dateStamp() {
